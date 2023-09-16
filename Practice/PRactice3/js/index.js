1cefe38aef90043a45bfefb8c4209e520760e6c9
Zylphyhr <144801565+Zylphyhr@users.noreply.github.com>
2023-09-16 15:10:59 +0200

    Test

--- Practice/PRactice3/js/index.js
+++ Practice/PRactice3/js/index.js
@@ -22,43 +22,50 @@ for( let x = 0; x < 20; x++ ) {
     arrayList.push( [Math.floor(Math.random()*100),Math.floor(Math.random()*100)] );
 }
 arrayList.map( x => x.push( x[0] + x[1] ) );
-document.getElementById("01").innerHTML = arrayList.map( x => `${x[0]} + ${x[1]} = ${x[2]}<br>`).join("");
+document.getElementById("01").innerHTML = arrayList
+    .sort( (a,b) => a[2] - b[2] )
+    .map( x => `${x[0]} + ${x[1]} = ${x[2]}<br>`)
+    .join("");
 
 const myGGNumber = Math.floor(Math.random()*100)+1;
 let GGLow = 0; let GGHigh = 101; let guesses = 0;
-
+let ggLabel = `Guess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`;
 document.getElementById("guessingGame").onclick = function() {
     let guess = 0;
     const message = document.getElementById("02");
-    while( guess = Number( prompt(`Guess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`) ) ) {
+    while( guess = Number( prompt(ggLabel) ) ) {
         guesses++;
         console.log( guess );
         if (isNaN(guess) || typeof guess != 'number') {
             console.log( "Bad Number" );
-            message.innerHTML += "<br>Guess wasted on a num number.  Try again.<br>";
+            ggLabel = `Guess wasted on a num number.  Try again.\nGuess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`;
             continue;
         }
         if( GGLow === GGHigh ) { console.log( `ERROR: ${myGGNumber}`); break; }
         if (guess <= GGLow || guess >= GGHigh) {
             console.log( "Outside of High/Low" );
-            message.innerHTML += `Guess outside of parameters of ${GGLow} and ${GGHigh}`;
+            ggLabel = `Guess outside of parameters of ${GGLow} and ${GGHigh}\nGuess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`;
             continue;
         }
         if( guess < myGGNumber ) {
             console.log( "Guess is too low." );
             GGLow = guess;
-            message.innerHTML += '<br>My number is higher than that.  Try again.<br>';
+            ggLabel = `My number is higher than that.  Try again.\nGuess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`;
             document.getElementById( "ggLow" ).innerHTML = `${guess}`;
         } else if( guess > myGGNumber ) {
             console.log( "Guess is too high" );
             GGHigh = guess;
-            message.innerHTML += '<br>My number is lower than that.  Try again.<br>';
+            ggLabel = `My number is lower than that.  Try again.\nGuess #${guesses+1} (${GGLow+1} to ${GGHigh-1})`;
             document.getElementById( "ggHigh" ).innerHTML = `${guess}`;
         } else break;
     }
     if (guess === myGGNumber) {
-        message.innerHTML = `Success! My number was ${myGGNumber}! It took you ${guesses} guesses to figure out.`;
+        let box = document.createElement("div");
+        box.innerHTML += `<br>Success! My number was ${myGGNumber}! It took you ${guesses} guesses to figure out.<br>`;
+        message.appendChild(box);
     } else {
-        message.innerHTML = "You cancelled the game.";
+        let box = document.createElement("div");
+        box.innerHTML = "<br>You cancelled the game.<br>";
+        message.appendChild(box);
     }
 }
